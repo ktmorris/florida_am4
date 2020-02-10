81373116b4d034cfb22131ed10e086ebf4c61ec4
Kevin <kevin.morris@nyu.edu>
2020-02-10 17:00:40 -0500

    add package. add weighted t test

--- code/00_initialize.R
+++ code/00_initialize.R
@@ -1,3 +1,5 @@
+library(Hmisc)
+library(lubridate)
 library(lme4)
 library(scales)
 library(estimatr)
@@ -13,3 +15,16 @@ library(data.table)
 
 
 db <- dbConnect(SQLite(), "D:/national_file.db")
+
+weighted.ttest.ci <- function(x, weights, conf.level = 0.95) {
+  nx <- length(x)
+  df <- nx - 1
+  vx <- wtd.var(x, weights, normwt = TRUE) ## From Hmisc
+  mx <- weighted.mean(x, weights)
+  stderr <- sqrt(vx/nx)
+  tstat <- mx/stderr ## not mx - mu
+  alpha <- 1 - conf.level
+  cint <- qt(1 - alpha/2, df)
+  cint <- tstat + c(-cint, cint)
+  cint * stderr
+}
