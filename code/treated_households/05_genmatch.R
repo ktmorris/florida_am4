ba0f6e491ff3976fc0ac9c65f1b7a0879ec383fa
Kevin <kevin.morris@nyu.edu>
2020-02-19 18:13:11 -0500

    make sampling more efficient. change genmatch to include congressional district exact match

--- code/treated_households/05_genmatch.R
+++ code/treated_households/05_genmatch.R
@@ -25,15 +25,13 @@ if(on_nyu){
 
 fl_file_pre_match <- readRDS("./temp/fl_file_pre_match.rds") 
 
-samp <- rbind(
-  filter(fl_file_pre_match, treated == 1) %>% 
-    sample_frac(0.05),
-  filter(fl_file_pre_match, !treated) %>% 
-    sample_frac(0.05)
-)
+samp <- fl_file_pre_match %>% 
+  group_by(treated) %>% 
+  sample_frac(0.05)
 
 match_data <- samp %>% 
-  select(-LALVOTERID, -treated, -max_release, -US_Congressional_District)
+  select(-LALVOTERID, -treated, -max_release)
 
-genout <- GenMatch(Tr = samp$treated, X = match_data, replace = T, cluster = cl)
+genout <- GenMatch(Tr = samp$treated, X = match_data, replace = T, cluster = cl,
+                   exact = c(rep(T, 6), F, F, T, T, F, F, T), pop.size = 1000)
 saveRDS(genout, "./temp/genout_t1.rds")
