be1b1a7d275d23d89456728f59ed5082bba62e4e
Kevin <kevin.morris@nyu.edu>
2021-02-05 09:36:58 -0500

    only allow for running on nyu hpc. set seed for replication

--- code/treated_households/06_genmatch.R
+++ code/treated_households/06_genmatch.R
@@ -1,37 +1,42 @@
-## this can be run locally or on NYU's HPC. Set option in next step
-## option allowed because of how long GenMatch can take
 
-on_nyu <- F
+## needs to be run on nyu hpc or its way too slow
+## set seed for reproducibility. this is a random 5-digit number: floor(runif(1)*100000)
+## on nyu hpc with 50 processors, 50gb RAM: Total run time : 66 hours 14 minutes and 25 seconds
+set.seed(85132)
 
-if(on_nyu){
-  library(Matching)
-  library(data.table)
-  library(snow)
-  library(parallel)
-  library(scales)
-  library(kableExtra)
-  library(tidyverse)
-  
-  setwd("/scratch/km3815/matching")
-  
-  NodeFile = Sys.getenv("MY_HOSTFILE")
-  
-  
-  cl <- makeCluster(c(readLines(NodeFile)), type="SOCK")
-}else{
-  source("./code/misc/AutoCluster4.R")
-  cl <- NCPUS(detectCores() - 1)
-}
+library(Matching)
+library(data.table)
+library(scales)
+library(kableExtra)
+library(tidyverse)
+require(snow)
+require(parallel)
 
-fl_file_pre_match <- readRDS("./temp/fl_file_pre_match.rds") 
+
+Sys.info()
+
+NodeFile = Sys.getenv("MY_HOSTFILE")
+
+print(NodeFile)
+
+readLines(NodeFile)
+
+cl<-makeCluster(c(readLines(NodeFile)), type="SOCK")
+cl
+
+
+fl_file_pre_match <- readRDS("temp/fl_file_pre_match.rds") 
 
 samp <- fl_file_pre_match %>% 
   group_by(treated) %>% 
-  sample_frac(0.05)
+  sample_frac(0.05) %>% 
+  ungroup()
 
 match_data <- samp %>% 
   select(-voter_id_anon, -treated, -max_release)
 
 genout <- GenMatch(Tr = samp$treated, X = match_data, replace = T, cluster = cl,
                    exact = c(rep(T, 6), F, F, T, T, F, F, T), pop.size = 1000)
-saveRDS(genout, "./temp/genout_t1.rds")
+saveRDS(genout, "temp/genout_av.rds")
+
+stopCluster(cl)
